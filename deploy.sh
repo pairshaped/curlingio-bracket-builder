22177b6eeb775b87d3493a9dff5996509e1c5166
Dave Rapin <dave@rapin.com>
2019-12-29 19:46:07 -0500

    Added comment about force pushing github pages

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,6 @@ git add --all
 git commit -am "Pages build"
 git push
 git subtree push --prefix build origin gh-pages
+
+# If there's ever a merge issue because github added a file, we can force push over it.
+# git push origin `git subtree split --prefix build master`:gh-pages --force
