03043f2c758c1b530e09593ece5b247f76a359be
Dave Rapin <dave@rapin.com>
2019-12-29 19:20:46 -0500

    Added docs build for github pages

---
+++ docs/precache-manifest.8d21324af29d19f419463dd6507b449e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "4d5395427ae639f3197a0ef0986ac812",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "f2b8f2321ea470129ef4",
+    "url": "/static/css/main.97412b66.chunk.css"
+  },
+  {
+    "revision": "f2b8f2321ea470129ef4",
+    "url": "/static/js/main.f2b8f232.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "3ca81432027bbb7b376c",
+    "url": "/static/js/vendors~main.3ca81432.chunk.js"
+  }
+]);
