124743b1d4de78fcaeed0efc634785a422da1b55
Dave Rapin <dave@rapin.com>
2022-05-01 13:04:41 -0400

    Reduced opacity of the game being dragged to make it more obvious and to better show the underlying grid.

--- src/Main.elm
+++ src/Main.elm
@@ -1171,8 +1171,17 @@ viewCell model dragId dropId toCoords group row col =
 
 viewGame : Maybe DraggableId -> Maybe DroppableId -> List Game -> List Team -> Game -> Coords -> Html Msg
 viewGame dragId dropId games teams game onCoords =
+    let
+        dragging =
+            case dragId of
+                Just (DraggableGame coords) ->
+                    coords == game.coords
+
+                _ ->
+                    False
+    in
     div
-        ([ class "game", onDoubleClick (EditGame game) ] ++ DragDrop.draggable DragDropMsg (DraggableGame onCoords))
+        ([ classList [ ( "game", True ), ( "dragging-game", dragging ) ], onDoubleClick (EditGame game) ] ++ DragDrop.draggable DragDropMsg (DraggableGame onCoords))
         [ div
             [ class "d-flex game-header" ]
             [ div
@@ -1263,7 +1272,7 @@ viewResultConnectors dragId gameId =
     [ div
         ([ classList
             [ ( "game-result-connector", True )
-            , ( "dragging-item", dragging Winner )
+            , ( "dragging-connector", dragging Winner )
             ]
          ]
             ++ DragDrop.draggable DragDropMsg (DraggableResult ( gameId, Winner ))
@@ -1273,7 +1282,7 @@ viewResultConnectors dragId gameId =
     , div
         ([ classList
             [ ( "game-result-connector", True )
-            , ( "dragging-item", dragging Loser )
+            , ( "dragging-connector", dragging Loser )
             ]
          ]
             ++ DragDrop.draggable DragDropMsg (DraggableResult ( gameId, Loser ))
