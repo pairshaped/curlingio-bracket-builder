9133514185e9dae15df7c879446d79814aead695
Dave Rapin <dave@rapin.com>
2022-05-03 15:20:36 -0400

    Tweaks to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+gco deploy
+git merge master
 elm-app build
 touch build/.nojekyll
 cp CNAME build/CNAME
@@ -5,6 +7,7 @@ git add --all
 git commit -am "Pages build"
 git push
 git subtree push --prefix build origin gh-pages
+gco master
 
 # If there's ever a merge issue because github added a file, we can force push over it.
 # git push origin `git subtree split --prefix build master`:gh-pages --force
